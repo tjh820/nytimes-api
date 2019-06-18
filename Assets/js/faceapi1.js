88c3809a99b64f2216c097c1f8e199158c1d4b00
Tiffany Hollenbeck <tjh820@yahoo.com>
2019-06-18 10:54:43 -0400

    local storage added

--- Assets/js/faceapi1.js
+++ Assets/js/faceapi1.js
@@ -1,48 +1,65 @@
-var successCallback = function( data, textStatus, jqXHR ) {
-         var results = data.people[0].emotions
-          console.log(data);
-          var greatestEmotion = "anger"
-          var greatestEmotionNum = results.anger
-          for (keys in results  ){
-          if(results[keys]>greatestEmotionNum) {
-              greatestEmotionNum=results[keys]
-              greatestEmotion = keys
+var successCallback = function (data, textStatus, jqXHR) {
+  var results = data.people[0].emotions
+  console.log(data);
+  var greatestEmotion = "anger"
+  var greatestEmotionNum = results.anger
+  for (keys in results) {
+    if (results[keys] > greatestEmotionNum) {
+      greatestEmotionNum = results[keys]
+      greatestEmotion = keys
 
-          }   
+    }
 
-          }
-          console.log(greatestEmotion, greatestEmotionNum)
-          var genre
-          switch(greatestEmotion){
-              case "anger":
-              genre = 12
-              break;
-              case "happiness":
-              genre = 28
-              break;
-              default:
-              genre = 27
+  }
+  console.log(greatestEmotion, greatestEmotionNum)
+  localStorage.getItem('greatestEmotionNum')
+  var genre
+  switch (greatestEmotion) {
+    case "anger":
+      genre = 12, 37
+      break;
+    case "happiness":
+      genre = 28, 16
+      break;
+    case "disgust":
+      genre = 80, 35, 99, 14
+      break;
+    case "fear":
+      genre = 878, 12
+      break;
+    case "sadness":
+    genre = 10749, 36
+    break;
+    case "surprise":
+    genre = 10752, 9648
+    break;
 
-          }
+
+  }
 
 
 
-          randomMovie(genre)
-        };
-        
-        var failCallback = function( jqXHR, textStatus, errorThrown ) {
-          alert( "There has been an error!" );
-        };
+  randomMovie(genre)
+};
 
-        $( "#myform" ).submit(
-          function(e) {
-            e.preventDefault();
-            var formdata = new FormData( document.getElementById( "myform" ) );
-            $.ajax( { url         :  $( this ).attr( 'action' ),
-                      type        : "POST",
-                      data        : formdata,
-                      success     : successCallback,
-                      error       : failCallback,
-                      processData : false,
-                      contentType : false } );
-          });
+var failCallback = function (jqXHR, textStatus, errorThrown) {
+  alert("There has been an error!");
+};
+
+$("#myform").submit(
+  function (e) {
+    e.preventDefault();
+    var formdata = new FormData(document.getElementById("myform"));
+    $.ajax({
+      url: $(this).attr('action'),
+      type: "POST",
+      data: formdata,
+      success: successCallback,
+      error: failCallback,
+      processData: false,
+      contentType: false,
+
+    });
+  });
+
+ 
