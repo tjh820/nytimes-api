ba3edd087dbd288a6ea3be9149e7ee165e4bbc0d
tyler <tylermarcus14@gmail.com>
2019-06-19 19:46:58 -0400

    working on youtubeAPI

--- Assets/js/youtubeAPI.js
+++ Assets/js/youtubeAPI.js
@@ -1,4 +1,11 @@
 <script src="https://apis.google.com/js/api.js"></script>
+<script>
+  /**
+   * Sample JavaScript code for youtube.search.list
+   * See instructions for running APIs Explorer code samples locally:
+   * https://developers.google.com/explorer-help/guides/code_samples#javascript
+   */
+
   function authenticate() {
     return gapi.auth2.getAuthInstance()
         .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
@@ -6,18 +13,17 @@
               function(err) { console.error("Error signing in", err); });
   }
   function loadClient() {
-    gapi.client.setApiKey("AIzaSyBzMimKJl4XObH_z0fu467SrC1kZ431-X4");
+    gapi.client.setApiKey("YOUR_API_KEY");
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function() { console.log("GAPI client loaded for API"); },
               function(err) { console.error("Error loading GAPI client for API", err); });
   }
-  var movie =  (localStorage.getItem("movieToWatch")+" Trailer");
   // Make sure the client is loaded and sign-in is complete before calling this method.
   function execute() {
     return gapi.client.youtube.search.list({
       "part": "snippet",
       "maxResults": 1,
-      "q": movie
+      "q": "Batman Trailer"
     })
         .then(function(response) {
                 // Handle the results here (response.result has the parsed body).
@@ -25,10 +31,9 @@
               },
               function(err) { console.error("Execute error", err); });
   }
-
   gapi.load("client:auth2", function() {
     gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
   });
-
-{/* <button onclick="authenticate().then(loadClient)">authorize and load</button>
-<button onclick="execute()">execute</button> */}
+</script>
+<button onclick="authenticate().then(loadClient)">authorize and load</button>
+<button onclick="execute()">execute</button>
