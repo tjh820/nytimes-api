570153fa06fb11ba7639907edad6ced97bcd8b22
Delane Chen <delane4.0@gmail.com>
2019-06-19 20:59:27 -0400

    premerge

--- Assets/js/manualmood.js
+++ Assets/js/manualmood.js
@@ -1,9 +1,19 @@
 function selectorSub(){
     let e = $("#selector :selected")
+    let userSer = JSON.parse(localStorage.getItem("userSer"))
     console.log(e)
+    if(e[0].value == ""){
+        //modul goes here
+        console.log("oops")
+    }
+    else if(userSer.amazon == false && userSer.netflix == false && userSer.itunes == false){
+        console.log("oops")
+    }
+    else{
+
     let emoStr = e[0].value
+    
     console.log(emoStr)
-
     choiceCallback(emoStr)
-
+    }
 }
