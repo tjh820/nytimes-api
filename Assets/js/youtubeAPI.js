40318226a04b4d8c6e659db75128547a693db59a
tyler <tylermarcus14@gmail.com>
2019-06-19 20:53:17 -0400

    got youtube working

--- Assets/js/youtubeAPI.js
+++ Assets/js/youtubeAPI.js
@@ -1,28 +1,18 @@
+var videoId
 
+function youtubeAPI(){  
+  var movie = localStorage.getItem("movieToWatch");
+  var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + movie +" Trailer" +"&key=AIzaSyBzMimKJl4XObH_z0fu467SrC1kZ431-X4";
 
-  function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
-  function loadClient() {
-    gapi.client.setApiKey("AIzaSyBzMimKJl4XObH_z0fu467SrC1kZ431-X4");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
-    return gapi.client.youtube.search.list({
-      "part": "snippet",
-      "maxResults": 1,
-      "q": "Batman Trailer"
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
+// Creating an AJAX call for the specific movie button being clicked
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+  videoId = response.items[0].id.videoId;
+  $("#utube").attr("src","https://www.youtube.com/embed/"+videoId+"?rel=0")
+});
+}
+
 
